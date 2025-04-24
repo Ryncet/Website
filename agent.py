1b5d14c91576eabf8790d466f10bfdc573714ca7
Ryncet <30adityaa@students.harker.org>
2025-04-23 18:49:44 -0700

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,29 @@
+import subprocess
+import webbrowser
+
+redirect_url = "https://www.pornhub.com"
+script = '''
+try
+    do shell script "echo Authenticated!" with administrator privileges
+    return "authenthicated"
+on error errMsg number errNum
+    return "failed"
+end try
+'''
+try:
+    result = subprocess.check_output(["osascript", "-e", script], text = True).strip()
+    if result == "authenticated":
+        print("✅ macOS authentication succesful")
+        subprocess.run(['osascript',"-e", 'set volume output volume 10'])
+        subprocess.run(["afplay", "p0rn-hub-into.mp3"])
+        webbrowser.open(redirect_url)
+    elif result == "failed":
+        print("❌ macOS authentication failed.")
+        subprocess.run(['osascript',"-e", 'set volume output volume 10'])
+        subprocess.run(["afplay", "p0rn-hub-into.mp3"])
+        webbrowser.open(redirect_url)
+except subprocess.CalledProcessError:
+    print("❌ macOS authentication failed.")
+    subprocess.run(['osascript',"-e", 'set volume output volume 10'])
+    subprocess.run(["afplay", "p0rn-hub-into.mp3"])
+    webbrowser.open(redirect_url)
